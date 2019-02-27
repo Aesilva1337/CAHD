9d82fe2c4e85ccbf8557233017c39afe75e8402e
Arthur Elidio da Silva <arthur.elidio.ae@gmail.com>
2019-02-26 22:31:33 -0300

    Ajustes gramaticais

--- interface/src/wizardSteps/StepOne.js
+++ interface/src/wizardSteps/StepOne.js
@@ -4,7 +4,7 @@ class StepOne extends Component {
   render() {
     return (
       <div>
-        <h1>Isso é um teste Para criação da wizard</h1>
+        <h1>Isso é um teste para criação da wizard</h1>
         <h3>StepOne</h3>
         <p>
           <button onClick={this.props.previousStep}>Previous Step</button>
--- interface/src/wizardSteps/StepTwo.js
+++ interface/src/wizardSteps/StepTwo.js
@@ -4,8 +4,8 @@ class StepTwo extends Component {
   render() {
     return (
       <div>
-        <h1>Isso é um teste Para criação da wizard</h1>
-        <h3>StepTwo</h3>
+        <h1>Isso é um teste para criação da wizard</h1>
+        <h3>StepTwo</h3>        
         <p>
           <button onClick={this.props.previousStep}>Previous Step</button>
         </p>
