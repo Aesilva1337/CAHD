6bf7d401b18839cc88859202a2e1e66903fee528
Leonardo Oliveira De Freitas <lofreitas2@stefanini.com>
2019-10-13 18:57:35 -0300

    Ajuste python

--- artificial_inteligence/config.py
+++ artificial_inteligence/config.py
@@ -1,10 +1,13 @@
 from os import getenv
 
+DATA_BACKEND = 'mongodb'
+MONGO_URI = 'mongodb://cahd:cahd@34.94.128.254:27017/CAHD'
+
 
 class Config:
     APP_PORT = int(getenv('APP_PORT'))
     DEBUG = eval(getenv('DEBUG').title())
-    MONGODB_HOST = getenv('MONGODB_URI')
+    MONGODB_HOST = getenv('MONGODB_URI', MONGO_URI)
 
 
 class DevelopmentConfig(Config):
